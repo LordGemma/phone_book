d56a0dd3adf302f4408a9b6d797fca4c366e7e17
Vadym_Aminov <vadym_aminov@epam.com>
2019-05-31 14:04:47 +0300

    add modal

--- src/Users/Components/User/UserDetailsComponents/CallHistoryItem.jsx
+++ src/Users/Components/User/UserDetailsComponents/CallHistoryItem.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const CallHistoryItem = ({ call }) => {
+  const {
+    conversation_direction,
+    conversation_time,
+    date,
+  } = call;
+  const callDirection = conversation_direction === 'out' ? '' : ;
   return (
     <div className="call">
       
